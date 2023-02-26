d7d358a59ced3e82407a3b5721f4b3a5ede49173
tgcidpl <tgcidpl@gmail.com>
2023-02-26 19:18:45 +0100

    addToCart WIP

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import Cart from "./modules/cart.js";
 import Delete from "./modules/delete.js";
 import Print from "./modules/print.js";
 
-const productsURL = 'https://shopisko-98e58-default-rtdb.firebaseio.com/products'
+const productsURL =
+  "https://shopisko-98e58-default-rtdb.firebaseio.com/products";
 
 Add(productsURL);
 Print(productsURL);
